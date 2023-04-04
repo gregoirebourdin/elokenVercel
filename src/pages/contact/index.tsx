25ff2153ad0e87b13797a3a5708774f3a7718e42
gregoire.eloken <gregoire@eloken.com>
2023-04-04 14:02:08 +0800

    Update index.tsx

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,26 +3,18 @@ import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 import {FormContact} from "@/templates/Contact";
 import {path} from "@/routes/paths";
-import {metas} from "@/routes/metas";
 
-const pages = [
-    {
-        name: "Contact",
-        href: path.contact.root,
-        current: true,
-    }
-]
-const meta = metas.contact;
 
 export default function Contact() {
     return (
         <>
-            <Meta
+            <h2>Contact</h2>
+    {/*        <Meta
                 title={meta.title}
                 description={meta.description}
                 keywords={meta.keywords}
-            />
-            <FormContact  pages={pages} meta={meta}/>
+            />*/}
+           {/* <FormContact  pages={pages} meta={meta}/>*/}
         </>
     );
 }
